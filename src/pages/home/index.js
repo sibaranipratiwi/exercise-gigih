f3b80a06036242ada226b3f6707f6fa4c2dcfad5
sibaranipratiwi <sibaranipratiwi@gmail.com>
2022-04-06 19:26:42 +0700

    Making and Calling Element

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+const Gif = () => {
+    return(
+        <div>
+            <h1>Search Your Favorite Gif</h1>
+            <input type="text" value="Type here"></input>
+            <button type='submit' value="search">Search</button>
+            <br></br>
+            <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"></img>
+        </div>
+    )
+}
+
+export default Gif;
